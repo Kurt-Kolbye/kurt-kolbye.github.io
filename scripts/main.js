c3ced62435a24058be00aeb1a9d2a4479fc6b7b3
Kurt Kolbye <kmkolbye@gmail.com>
2019-07-14 16:35:30 -0500

    Implemented SPA functionality for all nav items.

--- scripts/main.js
+++ scripts/main.js
@@ -3,6 +3,37 @@ $(document).ready(function() {
     function ViewModel() {
         var self = this;
 
+        self.templateView = ko.observable('Home');
+
+        // Navigate around the site
+        self.changeView = function(viewName) {
+            self.templateView(viewName);
+            self.updateActiveNav();
+        };
+
+        // Updates the current navigation item's class
+        self.updateActiveNav = function () {
+            // Remove the nav-active class from current nav item
+            $('.nav-active').removeClass('nav-active');
+
+            switch (self.templateView()) {
+                case 'Home':
+                    $('#nav-home').addClass('nav-active');
+                    break;
+                case 'About':
+                    $('#nav-about').addClass('nav-active');
+                    break;
+                case 'Projects':
+                    $('#nav-projects').addClass('nav-active');
+                    break;
+                case 'Contact':
+                    $('#nav-contact').addClass('nav-active');
+                    break;
+                default:
+                    break;
+            }
+        }
+
         // Small screen open/close of navigation bar
         self.toggleNav = function() {
             var navItems = $("#nav-items");
@@ -16,9 +47,9 @@ $(document).ready(function() {
             else {
                 navItems.slideDown();
             }
-        }
+        };
     };
     
-    ko.applyBindings(new ViewModel);
+    ko.applyBindings(new ViewModel());
 });
 
